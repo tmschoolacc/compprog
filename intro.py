2419074d7d377a4b7afc0596b886e65d280ce427
Scholar <scholar@paradigm.local>
2023-08-25 11:10:16 -0600

    my first push

---
+++ intro.py
@@ -0,0 +1,2 @@
+print("lorem ipsum")
+
---
+++ pirate.py
@@ -0,0 +1,7 @@
+greeting = input("Hello, possible pirate! What's the password?")
+
+if greeting in ("Arrr!"):
+	print("Go away, pirate.")
+
+elif greeting:
+    print("Greetings, hater of pirates!")
