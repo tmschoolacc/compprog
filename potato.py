3fbe3ea4ffcae8a1591d58f660a0bb4e840e3988
Scholar <scholar@paradigm.local>
2023-08-31 13:21:44 -0600

    adding pdm

---
+++ potato.py
@@ -0,0 +1,29 @@
+person = 'Daniel'
+
+for  i in range(10):
+    print(i)
+
+list = ['one','two','three']
+dictionary ={
+    'name': 'Daniel',
+    'age': 42
+}
+
+def is_even_number(number):
+    if number% 2 == 0:
+        print('This is an even number!')
+    else:
+        print('This is an odd number!')
+
+is_even_number(42)
+
+
+MAIN_CHARACTER = "Mario"
+
+main_character = "Luigi"
+
+myName = "Not Mr Hunt"
+
+my_name = "Johnny"
+
+MyName = "Doofus"
